4a4b1124a7b7a3069bcfb42db7823cb950b51df0
David Szabo <47574082+davidssabo@users.noreply.github.com>
2022-07-11 16:13:27 +0200

    switched back to using <img> in account-bar

--- components/account-bar.tsx
+++ components/account-bar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 const AccountBar = ({user}: {user: User}) => {
 	return <div className={style.bar}>
-		<Image src={user.imgURL} alt="Your profile picture" />
+		<img src={user.imgURL} alt="Your profile picture" />
 		<div>
 			<p>{user.name}</p>
 			<Link href="/api/oauth/logout">Sign out</Link>
